b38c31089912e381c9af04ca4e3003d74d6a729a
EC2 Default User <ec2-user@ip-172-31-32-218.ap-northeast-1.compute.internal>
2024-02-18 03:21:16 +0000

    [ADD]削除機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'post_images/new'
+  get 'post_images/index'
+  get 'post_images/show'
+  devise_for :users
+  
   get 'homes/top'
+  get 'homes/about', to: 'homes#about', as: 'about'
+
   root to: 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+
 end

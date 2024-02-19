1aa3b9a7ad61fe283c373beae21ff6843495065b
EC2 Default User <ec2-user@ip-172-31-32-218.ap-northeast-1.compute.internal>
2024-02-19 14:22:31 +0000

    [Finish]4-19

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -8,4 +8,12 @@ class PostImage < ApplicationRecord
   end
   has_one_attached :image
   belongs_to :user
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  get 'post_images/new'
-  get 'post_images/index'
-  get 'post_images/show'
+  
   devise_for :users
   
   get 'homes/top'
   get 'homes/about', to: 'homes#about', as: 'about'
 
   root to: 'homes#top'
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+   resource :favorite, only: [:create, :destroy]
+   resources :post_comments, only: [:create, :destroy]
+    
+  end
+  
+  resources :users, only: [:show, :edit, :update]
 end
